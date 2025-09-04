d5c528541b0f5279a4460703a8cc686ffa0357fd
Ansh Budhiraja <Anshbudhiraja77@gmail.com>
2025-09-04 16:37:45 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const User = require("./Model/User")
 const Menu = require("./Model/Menu");
 const app = express()
 const PORT = 3010
-app.use(express.json())
+app.use(express.json({limit:"100kb"}))
 app.use(cors())
 app.use("/uploads",express.static("uploads"))
 Connection()
